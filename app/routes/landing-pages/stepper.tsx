374b2f45917442e53c83ec50eb050efac46e5cd7
gonasi <gonasiapp@gmail.com>
2025-09-09 17:32:40 +0300

    draft ambulance

---
+++ app/routes/landing-pages/stepper.tsx
@@ -0,0 +1,22 @@
+export default function Stepper() {
+  return (
+    <section>
+      <section className="border border-gray-300 flex flex-row md:flex-col">
+        <div className="flex items-center space-x-2">
+          <img
+            src="https://img.funnelish.com/14613/761700/1742724704-logo-sign-icon-symbol-ambulance-car-art-mini-bus-design-graphic-vector-template-isolated-red_621257-117.jpg"
+            alt="ambulance"
+            className="h-30 w-30 object-contain"
+          />
+          <div>
+            <h2 className="text-2xl font-bold">Ambulance</h2>
+            <p className="text-gray-500">
+              According to the NHTSA, the U.S. has an ambulance response time
+              target of 8-12 minutes.
+            </p>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+}
